4f90155f47b90dcd502928e46fbdae0781fe14ae
Daisey Traynham <dtraynham@artlogic.com>
2013-10-12 16:08:10 +0200

    removed unnecessary files

--- untitled folder/assets/javascripts/.svn/text-base/single-column.js.svn-base
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    var isiOS = navigator.userAgent.match(/iPad|iPod|iPhone/i) != null;
-
-    if(isiOS) {
-        myScroll = new iScroll('wrapper');
-    }
-});
--- untitled folder/categories/scripts/get_cookie.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget --save-cookies cookie.txt --post-data '_method=POST&data%5BUser%5D%5Bemail_address%5D=kendall&data%5BUser%5D%5Bpassword%5D=MYKp01xk' https://abcschool.globalchalkboard.com/login
